5a9538794ac64934b40e5d59c4fa70751bf373ef
yallie <yallie@yandex.ru>
2024-11-14 02:56:51 +0300

    Added event stress tests.

--- test/Zyan.Tests/02_EventTests.cs
+++ test/Zyan.Tests/02_EventTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Xunit;
 using Zyan.Communication;
 using Zyan.Tests.Tools;
@@ -84,6 +83,52 @@ namespace Zyan.Tests
                 // both clients should get the event
                 Assert.True(await IsInTime(tcs1.Task));
                 Assert.True(await IsInTime(tcs2.Task));
+            }
+        }
+
+        [Test]
+        public async Task SyncTwoClientEventsStressTest()
+        {
+            using (var host = new ZyanComponentHost(HostConfig).RegisterComponent<IEventServer, EventServer>())
+            using (var conn1 = new ZyanConnection(ConnConfig))
+            using (var conn2 = new ZyanConnection(ConnConfig))
+            {
+                var max = 100;
+                var cnt1 = new AsyncCounter(max);
+                var proxy1 = conn1.CreateProxy<IEventServer>();
+                proxy1.MyEvent += (s, e) => cnt1.Increment();
+
+                var cnt2 = new AsyncCounter(max);
+                var proxy2 = conn2.CreateProxy<IEventServer>();
+                proxy2.MyEvent += (s, e) => cnt2.Increment();
+                proxy2.StressTest(max);
+
+                // both clients should get all the events
+                Assert.True(await IsInTime(cnt1.Task));
+                Assert.True(await IsInTime(cnt2.Task));
+            }
+        }
+
+        [Test]
+        public async Task AsyncTwoClientEventsStressTest()
+        {
+            using (var host = new ZyanComponentHost(HostConfig).RegisterComponent<IEventServer, EventServer>())
+            using (var conn1 = new ZyanConnection(ConnConfig))
+            using (var conn2 = new ZyanConnection(ConnConfig))
+            {
+                var max = 100;
+                var cnt1 = new AsyncCounter(max);
+                var proxy1 = conn1.CreateProxy<IEventServer>();
+                proxy1.MyEvent += (s, e) => cnt1.Increment();
+
+                var cnt2 = new AsyncCounter(max);
+                var proxy2 = conn2.CreateProxy<IEventServer>();
+                proxy2.MyEvent += (s, e) => cnt2.Increment();
+                await proxy2.StressTestAsync(max);
+
+                // both clients should get all the events
+                Assert.True(await IsInTime(cnt1.Task));
+                Assert.True(await IsInTime(cnt2.Task));
             }
         }
     }
