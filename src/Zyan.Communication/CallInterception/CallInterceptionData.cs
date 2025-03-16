28691fe362ca5072976444558d15a5cee3241352
yallie <yallie@yandex.ru>
2025-03-17 00:39:07 +0300

    Working on call interception.

---
+++ src/Zyan.Communication/CallInterception/CallInterceptionData.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Reflection;
+using Castle.DynamicProxy;
+
+namespace Zyan.Communication.CallInterception;
+
+// Delegate for remote method invocation.
+using InvokeRemoteMethodDelegate = Func<IInvocation, MethodInfo, object>;
+
+/// <summary>
+/// Describes a single call interception action.
+/// </summary>
+public class CallInterceptionData
+{
+    // Delegate for remote invocation
+    InvokeRemoteMethodDelegate _remoteInvoker = null;
+
+    // Remoting message invocation
+    IInvocation _methodInvocation = null;
+
+    /// <summary>
+    /// Creates a new instance of the CallInterceptionData class.
+    /// </summary>
+    /// <param name="invokerName">Inform interceptor about proxy unique name.</param>
+    /// <param name="parameters">Parameter values of the intercepted call</param>
+    /// <param name="remoteInvoker">Delegate for remote invocation</param>
+    /// <param name="methodInvocation">Remoting method invocation</param>
+    public CallInterceptionData(string invokerName, object[] parameters, InvokeRemoteMethodDelegate remoteInvoker, IInvocation methodInvocation)
+    {
+        InvokerUniqueName = invokerName;
+        Intercepted = false;
+        ReturnValue = null;
+        Parameters = parameters;
+        _remoteInvoker = remoteInvoker;
+        _methodInvocation = methodInvocation;
+    }
+
+    /// <summary>
+    /// Makes a remote call.
+    /// </summary>
+    /// <returns>Return value of the remotly called method.</returns>
+    public object MakeRemoteCall()
+    {
+        return _remoteInvoker(_methodInvocation, _methodInvocation.Method);
+
+        //var returnMessage = _remoteInvoker(_methodInvocation, _methodInvocation.Method);
+
+        //if (returnMessage != null)
+        //{
+        //    if (returnMessage.Exception != null)
+        //        throw returnMessage.Exception.PreserveStackTrace();
+
+        //    return returnMessage.ReturnValue;
+        //}
+
+        //return null;
+    }
+
+    /// <summary>
+    /// Proxy caller name.
+    /// </summary>
+    public string InvokerUniqueName { get; }
+
+    /// <summary>
+    /// Gets or sets wether the call was intercepted.
+    /// </summary>
+    public bool Intercepted { get; set; }
+
+    /// <summary>
+    /// Gets or sets the return value to be used.
+    /// </summary>
+    public object ReturnValue { get; set; }
+
+    /// <summary>
+    /// Gets or sets the parameters which are passed to the call.
+    /// </summary>
+    public object[] Parameters { get; set; }
+}
---
+++ src/Zyan.Communication/CallInterception/CallInterceptorCollection.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Castle.DynamicProxy;
+using Zyan.Communication.Toolbox;
+
+namespace Zyan.Communication.CallInterception;
+
+/// <summary>
+/// Collection of call interception devices.
+/// </summary>
+public class CallInterceptorCollection : ConcurrentCollection<CallInterceptor>
+{
+	/// <summary>
+	/// Creates a new instance of the CallInterceptorCollection class.
+	/// </summary>
+	internal CallInterceptorCollection()
+		: base()
+	{
+	}
+
+	/// <summary>
+	/// Adds call interceptors to the collection.
+	/// </summary>
+	/// <param name="interceptors">The interceptors to add.</param>
+	public void AddRange(IEnumerable<CallInterceptor> interceptors)
+	{
+		lock (SyncRoot)
+		{
+			foreach (var interceptor in interceptors)
+			{
+				base.Add(interceptor);
+			}
+		}
+	}
+
+	/// <summary>
+	/// Adds call interceptors to the collection.
+	/// </summary>
+	/// <param name="interceptors">The interceptors to add.</param>
+	public void AddRange(params CallInterceptor[] interceptors) =>
+		AddRange((interceptors ?? []).AsEnumerable());
+
+	/// <summary>
+	/// Finds a matching call interceptor for a specified method call.
+	/// </summary>
+	/// <param name="interfaceType">Componenet interface type</param>
+	/// <param name="uniqueName">Unique name of the intercepted component.</param>
+	/// <param name="remotingMessage">Remoting message from proxy</param>
+	/// <returns>Call interceptor or null</returns>
+	public CallInterceptor FindMatchingInterceptor(Type interfaceType, string uniqueName, IInvocation invocation)
+	{
+		if (Count == 0)
+			return null;
+
+		var matchingInterceptors = this
+			.Where(ic => ic.Enabled)
+			.Where(ic => ic.IsNameMatch(uniqueName))
+			.Where(ic => Equals(ic.InterfaceType, interfaceType))
+			.Where(ic => ic.MemberType == invocation.Method.MemberType)
+			.Where(ic => ic.MemberName == invocation.Method.Name)
+			.Where(ic => GetTypeList(ic.ParameterTypes) == GetTypeList(invocation.Method.GetParameters()));
+
+		lock (SyncRoot)
+		{
+			return matchingInterceptors.FirstOrDefault();
+		}
+	}
+
+	private string GetTypeList(Type[] types) =>
+		string.Join("|", types.Select(type => type.FullName).ToArray());
+
+	private string GetTypeList(ParameterInfo[] parameters) =>
+		string.Join("|", parameters.Select(p => p.ParameterType.FullName).ToArray());
+
+	///// <summary>
+	///// Creates call interceptor helper for the given interface.
+	///// </summary>
+	//public CallInterceptorHelper<T> For<T>()
+	//{
+	//	return new CallInterceptorHelper<T>(this);
+	//}
+}
